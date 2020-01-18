ee6e4d6bcf52349fe4a5abd9c45ba67b97d1998e
offiahchiaz <offiahchiaz@gmail.com>
2020-01-18 04:45:45 +0100

    Updated home blade to reflect the new UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/svg/freecodecamp.svg" style="height:100px;" alt="">
         </div>
+
+        <div class="col-9 pt-5">
+            <div>
+                <h1>freecodecamp</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>212</strong> following</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>153</strong> posts</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>
+                We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.
+            </div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4"><img src="/images/pic1.jpg" class="w-100" alt=""></div>
+        <div class="col-4"><img src="/images/pic3.jpg" class="w-100" alt=""></div>
+        <div class="col-4"><img src="/images/pic2.jpg" class="w-100" alt=""></div>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{-- {{ config('app.name', 'codersGram') }} --}}
-                    codersGram
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div> <img src="/svg/instagram.svg" class="pr-2" style="height: 20px; border-right: 1px solid #333" alt="codersGram logo"> </div>
+                    <div class="pl-2">codersGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
