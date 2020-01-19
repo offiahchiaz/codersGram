739ab3da34677952697d0cb31c36a7b45b09fe5e
offiahchiaz <offiahchiaz@gmail.com>
2020-01-19 23:33:11 +0100

    Added creat function, for creating post by authenticated user

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/p/create', 'PostsController@create')->name('p.create');
+Route::post('/p', 'PostsController@store')->name('p.store');
+
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
