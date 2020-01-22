2f2671c7e264ed0bd288057d62785d483757f46f
offiahchiaz <offiahchiaz@gmail.com>
2020-01-22 05:40:05 +0100

    Updated ProfilesController to handle profile image

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -22,7 +23,7 @@ class ProfilesController extends Controller
     public function update(User $user)
     {
         $this->authorize('update', $user->profile);
-        
+
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
@@ -30,7 +31,19 @@ class ProfilesController extends Controller
             'image' => '',
         ]);
 
-        auth()->user()->profile->update($data);
+        
+
+        if (request('image')) {
+            $imagePath = request('image')->store('profile', 'public');
+
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save();
+        }
+
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath]
+        ));
 
         return redirect("/profile/{$user->id}");
     }
