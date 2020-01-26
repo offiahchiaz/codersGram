6a6dafd5f861ae4b65a72d2c1cd20cee4f1b9949
offiahchiaz <offiahchiaz@gmail.com>
2020-01-26 01:39:44 +0100

    Added NewUserWelcomemail file

---
+++ resources/views/emails/welcom-email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Welcome to coders'Gram
+
+This is a community of fellow developer and we love that you have joined us.
+
+
+
+Warm Regards,<br>
+Chiazor
+@endcomponent
